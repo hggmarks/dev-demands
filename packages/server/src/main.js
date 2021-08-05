4a6080390063fcc6a858ce90c1a87b86a0275e45
hggmarks <hfm@discente.ifpe.edu.br>
2021-08-05 06:15:14 -0300

    add Apollo Server Express 3

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,24 +1,120 @@
 import express from 'express';
+import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express'
 
-const server = express();
+const startApolloServer = async () => {
 
+    const typeDefs = gql`
+        type Client {
+            id: ID!
+            name: String!
+        }
+
+        type Demand {
+            id: ID!
+            name: String!
+            client: Client!
+            deadline: String
+        }
+
+        type Query {
+            demands: [Demand]!
+        }
+    `;
+
+    const resolvers = {
+        Query: {
+            demands: () => [],
+        },
+    };
+
+    const server = new ApolloServer({ typeDefs, resolvers });
+    await server.start();
+
+    const app = express();
+    server.applyMiddleware({
+        app,
+        cors: { 
+            origin: 'http://localhost:3000',
+        },    
+    });
+
+    //await new Promise(resolve => app.listen({ port: 8000 }, resolve));
+    app.listen(8000, () => console.log(`Server ready at http://localhost:8000${server.graphqlPath}`))
+    console.log(`Server ready at http://localhost:8000${server.graphqlPath}`);
+    return {server, app};
+
+}
+
+/*
+const app = express();
+
+const server = new ApolloServer({
+    typeDefs: gql`
+
+        type Client {
+            id: ID!
+            name: String!
+        }
+
+        type Demand {
+            id: ID!
+            name: String!
+            client: Client!
+            deadline: String
+        }
+
+        type Query {
+            demands: [Demand]!
+        }
+    `,
+});
+
+server.applyMiddleware({
+    app,
+    cors: { 
+        origin: 'http://localhost:3000',
+    },
+});
+*/
+
+/*
 server.get('/status', (_, response) => {
     response.send({
         status: 'Okay',
     });
 });
 
-server.post('/authenticate', express.json(), (request, response) => {
-    console.log(
-        'Email', request.body.email,
-        'Senha', request.body.password
-    );
-    response.send();
-});
+const enableCors =  cors({ origin: 'http://localhost:3000'});
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
-const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+server
+    .options('/authenticate', enableCors)
+    .post('/authenticate', enableCors, express.json(), (request, response) => {
+        console.log(
+            'Email', request.body.email,
+            'Senha', request.body.pwd,
+        );
+        response.send({
+            Okay: true,
+        });
+    });
+*/
 
-server.listen(8000, '127.0.0.1', () => {
-    console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
-});
+
+
+//startApolloServer().server.listen(PORT, HOSTNAME, () => {
+//        console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
+//    });
+
+
+//const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+//const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+
+//const { server, app } = startApolloServer();
+
+//server.listen(PORT, HOSTNAME, () => {
+//    console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
+//});
+
+startApolloServer();
--- packages/web/src/pages/SignIn.js
+++ packages/web/src/pages/SignIn.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function SingIn() 
+export default function SingIn() {
+    
+    const [email, setEmail] = useState('');
+    const [pwd, setPwd] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('http://127.0.0.1:8000/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                pwd
+            })
+        }).then((response) => response.json())
+          .then((data) => {
+              console.log('Success!', data);
+          })
+    }
+
+    const handleEmailChange = (event) => setEmail(event.target.value);
+    const handlePwdChange = (event) => setPwd(event.target.value);
 
     return(
-        <form action="/authenticate" method="POST">
+
+        <form onSubmit={handleSubmit}>
             <fieldset>
-                <label for="email">Emaail</label>
-                <input type="email" id="email" name="email" inputmode="email" autocomplete="username" />
+                <label htmlFor="email">Email</label>
+                <input 
+                    type="email" 
+                    id="email" 
+                    inputMode="email" 
+                    autoComplete="username"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
             </fieldset>
             <fieldset>
-                <label for="password">Senha</label>
-                <input type="password" id="password" name="password" autocomplete="current-password" />
+                <label htmlFor="password">Senha</label>
+                <input 
+                    type="password"
+                    id="password" 
+                    autoComplete="current-password"
+                    value={pwd}
+                    onChange={handlePwdChange}
+                />
             </fieldset>
             <button type="submit">Entrar</button>
         </form>
